7bc7db059bc45029ecd0da032755a467bce21378
MatheusGmota <livematgreg@gmail.com>
2025-05-30 18:52:49 -0300

    first commit

---
+++ src/test/java/br/com/global_solutions/GlobalSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.global_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
